d71289061e1d34526e92319f46dd918fb994706a
Yash Shrivastava <shrivastavayash26@gmail.com>
2024-09-06 22:54:31 +0530

    B1__Agents_Setup: Create script to generate agent code.

---
+++ mscvt/src/mscvt/agents/add_visitor_agent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Adds n visitor agents to agents.py
+# by running ./this_script.sh n
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+FILE_PATH="$PARENT_DIR/agent.py"
+n=$1
+if [ $n -le 0 ]; then
+    exit 1
+fi
+
+echo from tools.visitor_tools import visitorTools >>"$FILE_PATH"
+for i in $(seq 1 $n); do
+    cat <<EOT >>"$FILE_PATH"
+
+visitor_$i = Agent(
+    role='Visitor',
+    goal="Meet host at the host's location",
+    tools=[visitorTools()],
+    memory=True,
+    verbose=True
+)
+
+EOT
+done
+echo "$n Visitor Agent code has been added to ../agent.py."
