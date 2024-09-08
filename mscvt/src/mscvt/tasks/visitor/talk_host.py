f808a7c4f6411f5d03f24316e5e3f782cceb5e2a
manan <manan@manan.myguest.virtualbox.org>
2024-09-08 21:57:11 +0530

    B3__Agent_Task_Creation: Add find_ci Task sh and py file , talk_host.py and add references to these files in notes.txt

---
+++ mscvt/src/mscvt/tasks/visitor/talk_host.py
@@ -0,0 +1,13 @@
+from crewai import Task
+# from mscvt.src.mscvt.tools.visitor_tools import visitorTools
+# from mscvt.src.mscvt.agent import Agent
+
+
+# for i in range(n):
+#     task = Task(
+#         description='Talk to the host about a random topic',
+#         agent=Agent[i],
+#         expected_output='A paragraph with 5 sentences about the topic',
+#         tools=[visitorTools]
+#     )
+
