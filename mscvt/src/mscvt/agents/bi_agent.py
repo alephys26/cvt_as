bac9f071e9ae6e3a1b76fe0fa3a46b3b043b219a
Yash Shrivastava <shrivastavayash26@gmail.com>
2024-09-14 23:38:10 +0530

    B1__ROS_Node_BI: Add type checking to BI files.

--- mscvt/src/mscvt/agents/bi_agent.py
+++ mscvt/src/mscvt/agents/bi_agent.py
@@ -3,7 +3,7 @@ import time
 
 
 class BI_Agent(Agent):
-    def __init__(self, map, residentList, authorisation, ID):
+    def __init__(self, map, residentList: dict[str, int], authorisation: dict[str, list[str]], ID: str):
         super().__init__(role='Building Incharge',
                          goal='To facilitate visitors to meet their intended host inside their building of care.',
                          memory=True,
@@ -20,18 +20,18 @@ class BI_Agent(Agent):
         # TODO: travel_time ###################
         self.travel_time = None
 
-    def getPath(self):
+    def getPath(self) -> dict[str, list[tuple[float, float, float]]]:
         # Run dijkstra algorithm on self.map
         # return a dict with resident from self.residentList as key and value as graph coordinates to move in path.
         pass
 
-    def chekHostPresence(self, host):
+    def chekHostPresence(self, host: str) -> bool:
         return host in self.residentList
 
-    def isVisitorAuthorized(self, host, visitor):
+    def isVisitorAuthorized(self, host: str, visitor: str) -> bool:
         return visitor in self.auth[host]
 
-    def isHostFree(self, host):
+    def isHostFree(self, host: str) -> bool:
         if self.meet[host] == None:
             return True
         if self.meet[host] <= time.time():
@@ -39,7 +39,7 @@ class BI_Agent(Agent):
             return True
         return False
 
-    def tellPath(self, host, visitor):
+    def tellPath(self, host: str, visitor: str) -> tuple[str, list[tuple[float, float, float]]]:
         if self.checkHostPresence(host):
             if self.isVisitorAuthorized(host, visitor):
                 if self.isHostFree(host):
@@ -53,15 +53,15 @@ class BI_Agent(Agent):
         else:
             return "DNE", None
 
-    def isOOS(self):
+    def isOOS(self) -> bool:
         return self.isHostFree(self.id)
 
-    def OOSHandler(self):
+    def OOSHandler(self) -> tuple[str, None]:
         remaining_oos_time = self.meet[self.id] + \
             self.travel_time - time.time()
         return f"OOS for {remaining_oos_time}", None
 
-    def run(self):
+    def run(self) -> tuple[str, list[tuple[float, float, float]]]:
         if self.isOOS():
             self.OOSHandler()
         return self.tellPath(self.host, self.visitor)
--- mscvt/src/mscvt_ros/mscvt_ros/bi_node.py
+++ mscvt/src/mscvt_ros/mscvt_ros/bi_node.py
@@ -1,13 +1,12 @@
 from mscvt.agents.bi_agent import BI_Agent as bia
+from mscvt.maps.building import Building
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
 class BIAgentNode(Node):
-    def __init__(self, building):
+    def __init__(self, building: Building):
         super().__init__('bi_agent_node')
-        self.building = building
-        # TODO: self.building_map
-        self.agent = bia()
+        self.agent = bia(building.map, building.residentList, building.authorisation, building.BI_Id)
         self.subscription = self.create_sunscription(String, 'ci_request', )
 
