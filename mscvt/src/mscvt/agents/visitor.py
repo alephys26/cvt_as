b69d125a911cf8fb3f605ec2a1e17676ce08dbfe
Yash Shrivastava <shrivastavayash26@gmail.com>
2024-09-16 14:17:27 +0530

    B1__ROS_Visitor_Node: Make complete visitor node and agent.

--- mscvt/src/mscvt/agents/visitor.py
+++ mscvt/src/mscvt/agents/visitor.py
@@ -1,9 +1,8 @@
 from crewai import Agent
-import time
 
 
 class Visitor(Agent):
-    def __init__(self, host:str, host_location:str, ID: str):
+    def __init__(self, host: str, host_location: str, ID: str):
         super().__init__(role='Visitor',
                          goal="Meet host at the host's location using CI's and BI's help.",
                          memory=True,
@@ -12,4 +11,3 @@ class Visitor(Agent):
         self.Id = ID
         self.host = host
         self.destination = host_location
-        self.findCI()
--- mscvt/src/mscvt_ros/mscvt_ros/visitor_node.py
+++ mscvt/src/mscvt_ros/mscvt_ros/visitor_node.py
@@ -2,7 +2,9 @@ from mscvt.agents.visitor import Visitor as vi
 from rclpy.node import Node
 from mscvt_messages.srv import Visitor
 from mscvt_messages.msg import findCI
-from math import sqrt
+from time import sleep
+import numpy as np
+from visualization_msgs.msg import Marker
 
 
 class Visitor_Node(Node):
@@ -11,13 +13,8 @@ class Visitor_Node(Node):
         self.agent = vi(host, host_location, ID)
         self.travelCount = 0
         self.coordinates = (0.0, 0.0, 0.0)
-
-        self.client = self.create_client(Visitor, 'ci_communication')
-        self.request = Visitor.Request()
-        self.request.visitorid = ID
-        self.request.hostid = host
-        self.request.hostlocation = host_location
-
+        self.setUpMarker()
+        self.setClient(host=host, host_location=host_location, ID=ID)
         self.pub = self.create_publisher(findCI, 'need_ci', 1)
         self.timer = self.create_timer(1.0, self.searchForCI)
         self.subs = self.create_subscription(
@@ -46,19 +43,18 @@ class Visitor_Node(Node):
             self.travel()
 
     def travel(self):
-        n = len(self.path)
-        i = 0
-        while i < n:
-            next = self.path[i]
-            grad = [0, 0, 0]
-            for dim in range(3):
-                grad[dim] = next[dim] - self.coordinates[dim]
-            normalizer = sqrt(sum([dim**2 for dim in grad]))
-            while not self.__compareCoordinates(next, self.coordinates):
-                for dim in range(3):
-                    self.coordinates[dim] += grad[dim] * \
-                        self.speed*0.5/normalizer
-            i += 1
+        for next_point in self.path:
+            grad = np.array(next_point) - np.array(self.coordinates)
+            normalizer = np.linalg.norm(grad)
+            if normalizer == 0:
+                continue
+            grad *= (self.speed * 0.5) / normalizer
+            while np.not_equal(next_point, self.coordinates):
+                self.coordinates = tuple(
+                    np.add(self.coordinates, grad).tolist())
+                self.publish()
+                sleep(0.5)
+
         self.travelCount += 1
         if self.travelCount == 1:
             self.request.hostlocation = 'INSIDE'
@@ -66,11 +62,33 @@ class Visitor_Node(Node):
             self.request.hostlocation = 'MAIN GATE'
         self.talkWithCI()
 
-    def __compareCoordinates(self, c1, c2):
-        if c1[0] != c2[0]:
-            return False
-        if c1[1] != c2[1]:
-            return False
-        if c1[2] != c2[2]:
-            return False
-        return True
+    def publish(self):
+        self.marker.header.stamp = self.get_clock().now().to_msg()
+        self.marker.pose.position = self.coordinates
+        self.marker_publisher.publish(self.marker)
+
+    def setUpMarker(self):
+        self.marker = Marker()
+        self.marker.header.frame_id = "map"
+        self.marker.ns = "visitor"
+        self.marker.id = 0
+        self.marker.type = Marker.SPHERE
+        self.marker.action = Marker.ADD
+        self.marker.pose.orientation.w = 1.0
+        self.marker.scale.x = 0.2
+        self.marker.scale.y = 0.2
+        self.marker.scale.z = 0.2
+        self.marker.color.r = 1.0
+        self.marker.color.g = 0.0
+        self.marker.color.b = 0.0
+        self.marker.color.a = 1.0
+        self.marker_publisher = self.create_publisher(
+            Marker, 'agent_location_marker', 10)
+        self.publish()
+
+    def setClient(self, ID: str, host: str, host_location: str):
+        self.client = self.create_client(Visitor, 'ci_communication')
+        self.request = Visitor.Request()
+        self.request.visitorid = ID
+        self.request.hostid = host
+        self.request.hostlocation = host_location
