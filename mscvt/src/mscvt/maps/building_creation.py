4de6f55ee9b29f5bc3ad7d29c9e7b9a654f23ecb
Anshu2000213 <araj90821@gmail.com>
2024-09-29 16:43:22 +0530

    B2_Building_Correction: Make correction from x_y,x_y to tuple

--- mscvt/src/mscvt/maps/building_creation.py
+++ mscvt/src/mscvt/maps/building_creation.py
@@ -2,12 +2,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class BuildingCreation:
-    def __init__(self, building_type, building_name,c_x,c_y):
+    def __init__(self, building_type, building_name,coordinate):
         self.building_type = building_type
         self.building_name = building_name
         self.cooridate_building={}
-        self.c_x=c_x
-        self.c_y=c_y
+        self.coordinate=coordinate
         self.G = nx.Graph()
 
     def create_building(self):
@@ -28,44 +27,44 @@ class BuildingCreation:
     def create_hostel(self):
         self.G.add_node(self.building_name)
         previous_node = self.building_name 
-        arr1=[-1,1,0]
-        arr2=[0,0,1]
+        arr1=[-0.1,0.1,0]
+        arr2=[0,0,0.1]
         for floor in range(1, 4):  
             floor_node = f'Floor {floor}'
-            self.cooridate_building[floor_node]=(self.c_x,self.c_y,floor)
+            self.coordinate[2]=floor
+            self.cooridate_building[floor_node]=self.coordinate
             self.G.add_edge(previous_node, floor_node, weight=1)  
             previous_node = floor_node  
 
-            
             for room in range(1, 4):  
                 room_node = f'Room {floor}0{room}'
-                self.cooridate_building[room_node]=(self.c_x+arr1[room-1],self.c_y+arr2[room-1],floor)
+                self.cooridate_building[room_node]=(self.coordinate[0]+arr1[room-1],self.coordinate[1]+arr2[room-1],floor)
                 self.G.add_edge(floor_node, room_node, weight=1)  
 
     def create_department(self):
-        arr1=[-1,1,0]
-        arr2=[0,0,1]
+        arr1=[-0.1,0.1,0]
+        arr2=[0,0,0.1]
         self.G.add_node(self.building_name)
         
         previous_node = self.building_name  
         for floor in range(1, 3):  
             floor_node = f'Floor {floor}'
-            self.cooridate_building[floor_node]=(self.c_x,self.c_y,floor)
+            self.cooridate_building[floor_node]=(self.coordinate[0],self.coordinate[1],floor)
             self.G.add_edge(previous_node, floor_node, weight=1)  
             previous_node = floor_node  
 
             for room in range(1, 3):  
                 room_node = f'Room {floor}0{room}'
-                self.cooridate_building[room_node]=(self.c_x+arr1[room-1],self.c_y+arr2[room-1],floor)
+                self.cooridate_building[room_node]=(self.coordinate+arr1[room-1],self.coordinate+arr2[room-1],floor)
                 self.G.add_edge(floor_node, room_node, weight=1)  
 
     def create_director(self):
         self.G.add_node(self.building_name)
         floor_node = f'Floor 1'
-        self.cooridate_building[floor_node]=(self.c_x,self.c_y,1)
+        self.cooridate_building[floor_node]=(self.coordinate[0],self.coordinate[1],1)
         self.G.add_edge(self.building_name, floor_node, weight=1)  
         room_node = 'Room 101'
-        self.cooridate_building[floor_node]=(self.c_x+0,self.c_y+1,1)
+        self.cooridate_building[floor_node]=(self.coordinate[0]+0,self.coordinate[1]+0.1,1)
         self.G.add_edge(floor_node, room_node, weight=1)  
 
     def get_graph(self):
